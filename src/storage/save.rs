235a4d272c65e7efe2a1afc134ce5acf7e00fbce
SO9010 <SO9010sami@gmail.com>
2024-12-20 00:25:06 +0000

    Update

--- src/storage/save.rs
+++ src/storage/save.rs
@@ -1,7 +1,11 @@
 use std::fs::{self, File};
+use std::io::{self, BufWriter, Write};
 use std::path::{Path, PathBuf};
-use image::ImageFormat;
+use image::io::Reader;
+use image::{DynamicImage, ImageFormat};
 use ureq;
+use webp::Encoder;
+use yt_dlp::model::format;
 
 use crate::api::webimage::url_to_buffer;
 use crate::api::yt::YouTubeClient;
@@ -35,14 +39,44 @@ pub fn download_audio(
 
             // Make this less hardcoded and more flexible to get iamges!
             if url.contains("archive.org") {
-
-                let parent = Path::new(url).parent().unwrap();
-                parent.join("__ia_thumb.jpg");
-
-           
-                let mut image_file = File::create(output_file.clone().with_extension("webp"))?;
-                // TODO FINISH THIS
-            }
+                let pos = url.rfind("/");
+                let (img, _) = url.split_at(pos.unwrap());
+                let img = img.to_owned() + "/__ia_thumb.jpg";
+        
+                log::info!("Downloading image {img}");
+        
+                let response = ureq::get(&img).call()?;
+                if response.status() != 200 {
+                    return Err(format!("Failed to download file: {}", response.status()).into());
+                }
+        
+                // Save the downloaded .jpg file
+                let mut p_osstr = output_file.clone().as_os_str().to_owned();
+                p_osstr.push(".jpg");
+                let jpg_path = std::path::Path::new(&p_osstr);
+        
+                let mut jpg_file = File::create(&jpg_path)?;
+                let mut reader = response.into_reader();
+                io::copy(&mut reader, &mut jpg_file)?;
+        
+                // Convert the .jpg file to .webp
+                log::info!("Converting image to WEBP format");
+                let img = Reader::open(jpg_path)?.decode()?;
+                let rgba_image = match img {
+                    DynamicImage::ImageRgba8(img) => img,
+                    other => other.to_rgba8(),
+                };
+        
+                let encoder = Encoder::from_rgba(rgba_image.as_raw(), rgba_image.width(), rgba_image.height());
+                let webp_data = encoder.encode(75.0); // Adjust quality if needed
+        
+                let webp_path = output_file.with_extension("webp");
+                let output_file = File::create(&webp_path)?;
+                let mut writer = BufWriter::new(output_file);
+                fs::remove_file(jpg_path);
+                writer.write_all(&webp_data)?;
+            }   
+            
             log::info!("Downloading book {book} to: {} from {url}", output_file.display());
                 
             let response = ureq::get(url).call()?;
@@ -58,7 +92,6 @@ pub fn download_audio(
     }
 
     Ok(output_file)
-}
-pub fn save_progress(book: &str, chapt: i32, url: &str) -> Result<(), Box<dyn std::error::Error>> {
+}pub fn save_progress(book: &str, chapt: i32, url: &str) -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
